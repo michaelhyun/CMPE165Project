8cc8e9adc95cdd9a2b9737247b6a705849146d9d
Khoa <khoavo910@gmail.com>
2018-03-10 22:16:29 -0800

    Can route between pages now

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  root 'home#index', as:'home'
+
+  get 'about' => 'pages#about', as: 'about'
+  get 'reservation' => 'pages#reservation', as: 'reservation'
+  get 'signup' => 'pages#signup', as: 'signup'
 end
