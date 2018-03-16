e51302825f44642b140790ee04610f0e762b0d9d
GurvGill <gurvir24@ymail.com>
2018-03-15 19:03:30 -0700

    models and controllers

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,8 @@
+class ReservationsController < ApplicationController
+    def new
+
+    end
+    def show
+        
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'home#index', as:'home'
 
   get 'about' => 'pages#about', as: 'about'
-  get 'reservation' => 'pages#reservation', as: 'reservation'
+  get 'reservation' => 'reservations#reservation', as: 'reservation'
   get 'signup' => 'pages#signup', as: 'signup'
 end
