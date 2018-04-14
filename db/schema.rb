d8e5c049d3d45c4ac28533e6421336397feb4a3d
thao <tranth916@gmail.com>
2018-04-13 22:43:18 -0700

    Search by locaion works in Hotel_list - Fetching Results From google PLACES API

--- db/schema.rb
+++ db/schema.rb
@@ -10,54 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180403162106) do
-
-  create_table "bookings", force: :cascade do |t|
-    t.string "firstname"
-    t.string "lastname"
-    t.string "confirmation"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "reservations", force: :cascade do |t|
-    t.string "roomtype"
-    t.integer "numberofbeds", default: 1
-    t.string "hotelname"
-    t.datetime "checkindate"
-    t.datetime "checkoutdate"
-    t.boolean "hasinternet", default: true
-    t.boolean "hasparking", default: true
-    t.boolean "nonsmoking", default: true
-    t.datetime "lastdatetocancel"
-    t.boolean "isreserved", default: false
-    t.string "confirmationcode", default: "Not Reserved"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "imgurl"
-    t.integer "price"
-    t.string "city"
-    t.string "state"
-    t.string "country"
-    t.integer "reservedbyuserid"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
