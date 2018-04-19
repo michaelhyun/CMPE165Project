fd0333dc5e986d8c86673add3392053c86ebeaf6
vincephng <vincephng@gmail.com>
2018-04-19 14:52:54 -0700

    add static page for booking, reservation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 root 'home#index'
@@ -9,6 +10,9 @@ get 'hotel_listing' => 'hotel#hotel_list'
 get 'hotel' => 'hotel#hotel_detail'
 get 'hotel_booking' => 'hotel#hotel_booking'
 get 'booking_complete' => 'hotel#booking_complete'
+
+get 'my_booking'=>'account#mybooking'
+get 'my_reservation'=>'account#myreservation'
 
 
 get 'aboutus' => 'admin#about_us'
