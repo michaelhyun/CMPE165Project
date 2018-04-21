c6fcb08ddc53c61f59f722407c4d8e2161ca20bc
Brandon Cecilio <brandonacecilio@gmail.com>
2018-04-21 03:56:25 -0700

    removed stray charges routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-# route to charges controller
-resources :charges, only: [:new, :create]
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
