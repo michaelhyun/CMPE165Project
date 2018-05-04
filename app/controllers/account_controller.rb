bd51bbe446ef54876d8f34435edc629b20364dc6
Brandon Cecilio <brandonacecilio@gmail.com>
2018-05-04 15:45:23 -0700

    can now display all bookings for current user

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -3,21 +3,6 @@ class AccountController < ApplicationController
   end
 
   def myreservation
-    curr_user_id = current_user.id
-
-    #curr_user_id = !params[:current_user_id].nil? ? params[:current_user_id] : ''
-    @results = Hotel.all.where("reserved_by LIKE ?", curr_user_id)
-
-
-
-    #   if !params[:location].nil?
-    #       @results_from_db = Hotel.all.where("city_name LIKE ?", params[:location])
-    #       if @results_from_db.nil? or @results_from_db.count == 0
-    #           puts("Querying from GOOGLE PLACES.")
-    #           add_results_to_db(params[:location])
-    #           @results_from_db = Hotel.all.where("city_name LIKE ?", params[:location])
-    #       end
-    #       @results = @results_from_db
-    #   end
+    @results = Booking.where(user_id: current_user.id)
   end
 end
