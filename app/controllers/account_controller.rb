f15f097eb172d7b68b4e77dae811221b4267744d
Brandon Cecilio <brandonacecilio@gmail.com>
2018-05-04 14:13:01 -0700

    changed reservations page to match hotel listing page

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -3,5 +3,14 @@ class AccountController < ApplicationController
   end
 
   def myreservation
+      if !params[:location].nil?
+          @results_from_db = Hotel.all.where("city_name LIKE ?", params[:location])
+          if @results_from_db.nil? or @results_from_db.count == 0
+              puts("Querying from GOOGLE PLACES.")
+              add_results_to_db(params[:location])
+              @results_from_db = Hotel.all.where("city_name LIKE ?", params[:location])
+          end
+          @results = @results_from_db
+      end
   end
 end
